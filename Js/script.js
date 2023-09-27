18684137ff257227c6c50caef5a1e9d322c18c46
Kishan Soni <hitesh.net1689@gmail.com>
2023-09-26 19:14:07 -0700

    Added html and script file

---
+++ Js/script.js
@@ -0,0 +1,182 @@
+
+// STEP 1: Convert the following highlighted identifiers to Camel Case notation:
+// let someMonth
+// function theMonth() { }
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+
+
+// STEP 2 : Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+// let numericLiteral = 691
+// let stringLiteral = 'Hello'
+// let booleanLiteral = true
+// let nullLiteral = null
+
+
+
+// Step 3 : Give me two examples of complex / variable expressions.
+// let x = 15
+// let y = 5
+// let complexExpression  = (x * 2 + y) / (x - 3)
+// console.log(complexExpression)
+
+
+
+// Step 4: Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+// let strFirstName;       // String variable for First Name
+// let strLastName;        // String variable for Last Name
+// let strAddress;         // String variable for Address
+// let strCity;            // String variable for City
+// let strState;           // String variable for State
+// let strZipCode;         // String variable for Zip Code
+// let numYourAge;         // Numeric variable for Your Age
+// let strReferralSource;  // String variable for Referral Source
+// let boolMayContactYou;  // Boolean variable for May We Contact You
+
+
+
+// Step 5: Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables.
+
+// 1st way: Using Separate Lines:
+// let strFirstName;
+// strFirstName = "Hiteshkumar";
+// let numYourAge;
+// numYourAge = 33;
+// let boolMayContactYou;
+// boolMayContactYou = true;
+
+// 2nd way:Using a Single Line:
+// let strFirstName = "Hiteshkumar";
+// let numYourAge = 33;
+// let boolMayContactYou = false;
+
+// 3rd way: Using Prompt:
+// let strFirstName = prompt("Please enter your first name:");
+// let numYourAge = parseInt(prompt("Please enter your age:"));
+// let boolMayContactYou = confirm("May we contact you?");
+
+
+
+// Step 6: Create a variable. Add a number and a string and display the coerced result in the browser’s console window.
+// let numberValue = 33;
+// let stringValue = " Hello";
+// let result = numberValue + stringValue;
+// console.log(result);
+
+// Step 7: Create two variables.For the first variable, add a Boolean and a string and display the coerced result.For the second variable, add a number and a Boolean and display the coerced result.
+// Adding a Boolean and a String:
+// let booleanValue = true;
+// let stringValue = " Hello";
+// let firstResult = booleanValue + stringValue;
+// console.log(firstResult);
+
+// Adding a Number and a Boolean:
+// let numberValue = 33;
+// let booleanValue = true;
+// let secondResult = numberValue + booleanValue;
+// console.log(secondResult);
+
+
+
+// Step 8: Fix invalid string literal
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString);
+
+
+
+// Step 9: Create a variable that produces a null value in the console window.Then, create a variable that produces an undefined value in the console window.
+// let nullValue = null;
+// console.log(nullValue);
+// let undefinedValue;
+// console.log(undefinedValue);
+
+
+
+// Step 10: Use the unary typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+// console.log(typeof "Hello"); // string
+// console.log(typeof 42); // number
+// console.log(typeof true); // boolean
+// console.log(typeof (Object)); // function
+// console.log(typeof undefinedVariable); // undefined
+
+
+
+// Step 11:
+// alert('Hello ' + 'Hiteshkumar Patel' + ' , welcome to the JavaScript class!')
+
+
+
+// Step 12: Declare a variable called name and set it equal to your name. Substitute your name in the previous alert string with the variable instead.
+// let myName = 'Hiteshkumar Patel'
+// alert('Hello ' + myName + ' , welcome to the JavaScript class!')
+
+
+
+// Step 13: Declare a variable called course and set it equal to “JavaScript”.Rework your alert string so that it displays the string of text but using the variables as opposed to hard coded text.
+// let course = 'JavaScript'
+// alert('Hello ' + 'Hiteshkumar Patel' + ' , welcome to the ' + course + ' class!')
+
+
+
+// Step 14
+// alert('Hello Zak Ruvalcaba.' + '\nWelcome to the JavaScript class!')
+
+
+
+// Step 15: Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response will now be captured in the name variable.
+// let myName = prompt('Please enter your name')
+// alert('Hello ' + myName + ', welcome to the JavaScript class!')
+
+
+
+// Step 16: Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking. The prompt’s response will now be captured in the course variable.
+// let course = prompt("Please enter the name of the class you are taking:")
+// let message = "Hello Hiteshkumar, welcome to the " + course + " class!";
+// alert(message);
+
+
+
+// Step 17: Declare a variable called x and assign it a value of 10.Declare a variable called y and assign it a value of 20.Display the sum of those two numbers in the console window.
+// let x = 10
+// let y = 20
+// let sum = x + y
+// console.log("The sum of " + x + " and " + y + " is " + sum)
+
+
+
+// Step 18: Declare a variable called x and assign it a value of 20.Add and assign 20 to that variable and display the result in the console window.The result should be 40.
+// let x = 20
+// x = x + 20
+// console.log("The result is: " + x)
+
+
+
+// Step 19: Declare a variable called x and assign it a value of 20.Multiply and assign 5 to that variable and display the result in the console window.The result should be 100.
+// let x = 20
+// x = x * 5
+// console.log("The result is: " + x)
+
+
+
+// Step 20: Declare a variable called x and assign it a value that equals the remainder of 20 divided by 3.Divide and assign 1 to that variable and display the result in the console window.The result should be 2. If you got 6.66 try again.
+// let x = 20 % 3
+// x = x / 1
+// console.log("The result is: " + x)
+
+
+
+// Step 21: Using a set of Comparison and Logical operators, write an application that evaluates to true and displays the result within the console window.
+// let a = 10;
+// let b = 5;
+// let result = (a > b) || (a != b);
+// console.log("The result is: " + result);
+
+
+
+// Step 22: Using a set of Comparison and Logical operators, write an application that evaluates to false and displays the result within the console window. The application cannot use the same operators used in the previous application.
+// let a = 15;
+// let b = 30;
+// let result = (a > b) && (a === b);
+// console.log("The result is: " + result);
